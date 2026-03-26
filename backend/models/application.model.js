1a9413ea729ecb7016d323ccefc67c01bceddb6c
Anuj <codeasj@outlook.com>
2026-03-27 01:30:18 +0530

    auth done

---
+++ backend/models/application.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const interviewSchema = new mongoose.Schema({
+    round: String,
+    date: Date,
+    notes: String,
+    outcome: {
+        type: String,
+        enum: ["passed", "failed", "pending"],
+        default: "pending"
+    }
+});
+
+const applicationSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    jobDescription:String,
+    status: {
+        type: String,
+        enum: ["applied", "screening", "interview", "offer", "rejected", "ghosted"],
+        default: "applied",
+    },
+    appliedDate: {
+        type: Date,
+        default: Date.now,
+    },
+    interviews: [interviewSchema],
+    notes: String,
+    Salary: String,
+     
+},{timestamps: true});
+
+export default mongoose.model("Application", applicationSchema);
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import authRoutes from "./routes/auth.routes.js";
+// import applicationRoutes from "./routes/application.routes.js";
+
+dotenv.config();
+const app = express();
+
+//Middleware
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/applications", applicationRoutes);
+
+// Health check
+app.get("/", (req, res) => res.json({ status: "API running" }));
+
+// Start
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
